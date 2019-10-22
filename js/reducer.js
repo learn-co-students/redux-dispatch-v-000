a1d37ef75b8eafb28d0b6f0b1407637d22037964
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-10-22 12:57:36 +0000

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,20 @@
+let state = {count: 0};
+ 
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+ 
+function dispatch(action){
+  state = changeState(state, action)
 }
-
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+ 
+dispatch({type: 'INCREASE_COUNT'})
+  // => 1
+dispatch({type: 'INCREASE_COUNT'})
+  // => 2
+dispatch({type: 'INCREASE_COUNT'})
