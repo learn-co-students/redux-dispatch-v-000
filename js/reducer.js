88d9d104f58e304717a0488d0f21200dddab289f
gitluky <jluk87@gmail.com>
2020-01-22 03:47:03 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,4 +1,4 @@
-function changeState(state, action){
+function changeState(state = { count: 0 }, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
       return {count: state.count + 1}
@@ -7,7 +7,7 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
+let state;
 let action = {type: 'INCREASE_COUNT'}
 
 function dispatch(action) {
@@ -18,3 +18,5 @@ function dispatch(action) {
 function render() {
   document.body.textContent = state.count;
 }
+
+dispatch({ type: 'beef' })
