a273d281ae228c6b82f0259657b9cab501e94729
emanao <emanao@me.com>
2021-05-26 16:14:45 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,6 @@
+let state = {count: 0};
+let action = {type: 'INCREASE_COUNT'};
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -6,8 +9,14 @@ function changeState(state, action){
       return state;
   }
 }
+ 
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
+function render(){
+    document.body.textContent = state.count
+}
+ 
+render()
